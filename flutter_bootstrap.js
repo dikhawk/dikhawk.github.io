05bcef2924e818fbc06172dd637948a5efdf51b1
dikhawk <dik_hawk@mail.ru>
2025-08-21 19:58:52 +0600

    added rustore icon

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3764956323"
+    serviceWorkerVersion: "3333090031"
   }
 });
